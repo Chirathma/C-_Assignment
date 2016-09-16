edc2fe0c9b3e231897f9d14939b69487315ee527
Devni-C <devniperera14@gmail.com>
2016-09-17 01:56:39 +0530

    features added

--- main.cpp
+++ main.cpp
@@ -1,98 +1,175 @@
 #include<iostream>
 #include<fstream>
 #include<string.h>
-
 #include <cstdlib> // used to exit
 using namespace std;
 
-//void transact();
-class Transaction;
-
-class Menu : public Transaction
+class Account
 {
-protected:
+private:
+    string userId;
+    double balance;
+public:
+    Account(string id, double balanceValue)
+    {
+        userId = id;
+        balance = balanceValue;
+    }
+    double getBalance ()
+    {
+        return balance;
+    }
+    void deposit(double amount)
+    {
+        balance += amount;
+    }
+
+    bool withdraw(double amount)
+    {
+        if(amount < balance)
+        {
+            balance -= amount;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
+
+//selecting main menu
+class Menu
+{
+private:
     string userID; //User ID: It can be contained string characters
     int pwd; //Password: It must contain only integer numbers
-    int option1;
+    int option, option1; //used as the switch-case operator
+    double withdrawValue;
+    double depositValue;
+    double balance;
+
+    Account* login()
+    {
+        cout << "---Login---\n\n";
+        cout << "Enter ID: ";
+        cin >> userID;
+        cout << "\nEnter Password: ";
+        cin >> pwd;
+
+        //Read the file named userID
+        //Match password
+        //If password is correct, create an Account object
+        double balance = 1500;
+        Account account(userID, balance);
+        return &account;
+    }
+
+    /*void transact(Account* account)
+    {
+        cout<< "\nEnter your requirement: "
+            << "\nBalance-'1'\t Deposit-'2'\t Withdraw-'3'\n";
+        cin>> option;
+        switch(option)
+        {
+            case 1:
+                balance = account->getBalance();
+                cout<< "\nYour balance is  "<<balance<< endl;
+                break;
+
+            case 2:
+                cout<< "\nEnter the amount of deposit: ";
+                cin>> depositValue;
+                account->deposit(depositValue);
+                break;
+
+            case 3:
+                cout<< "\nEnter the amount of withdraw: ";
+                cin>> withdrawValue;
+                account->withdraw(withdrawValue);
+                break;
+
+            default:
+                cout<< "\n--INVALID INPUT !!!--\n";
+        }
+    }*/
+
+    void createAcc()
+    {
+        cout << "---Create New Account---\n\n";
+        cout << "Enter User ID: ";
+        cin >> userID;
+        cout << "\nEnter the password: ";
+        cin >> pwd;
+        cout << "\n Re-enter the password: ";
+        cin >> pwd;
+    }
+
+    void quitAcc()
+    {
+        cout << "~~Thank You! Come Again!~~\n\n";
+    }
+
 public:
-    Transaction txn;
     void selectMenu()
     {
         cout << "1 - Login \n2 - Create New Account \n3 - Quit\n";
         cin >> option1;
-        switch(option1)
+        //Account* accountPointer;//=login();
+        int option;
+        switch (option1)
         {
         case 1:
-            cout << "---Login---\n\n";
-            cout << "Enter User ID: ";
-            cin >> userID;
-            cout << "\nEnter Password: ";
-            cin >> pwd;
-            txn.transact();
-            break;
+        {
+            Account* accountPointer= login();
+            cout << accountPointer->getBalance();
+            if(accountPointer != NULL)
+            {
+                cout<< "\nEnter your requirement: "
+                    << "\nBalance-'1'\t Deposit-'2'\t Withdraw-'3'\n";
+                cin>> option;
+                switch(option)
+                {
+                case 1:
+                {
+                    balance = accountPointer->getBalance();
+                    cout<< "\nYour balance is  "<<balance<< endl;
+                }
+                break;
+
+                case 2:
+                    cout<< "\nEnter the amount of deposit: ";
+                    cin>> depositValue;
+                    accountPointer->deposit(depositValue);
+                    break;
+
+                case 3:
+                    cout<< "\nEnter the amount of withdraw: ";
+                    cin>> withdrawValue;
+                    accountPointer->withdraw(withdrawValue);
+                    break;
+
+                default:
+                    cout<< "\n--INVALID INPUT !!!--\n";
+                }
+            }
+        }
+        break;
+
         case 2:
-            cout << "---Create New Account---\n\n";
-            cout << "Enter User ID: ";
-            cin >> userID;
-            cout << "\nEnter the password: ";
-            cin >> pwd;
-            cout << "\n Re-enter the password: ";
-            cin >> pwd;
-            txn.transact();
+            createAcc();
             break;
+
         case 3:
-            cout << "~~Thank You! Come Again!~~\n\n";
+            quitAcc();
             break;
+
         default:
             cout << "--INVALID INPUT!-- \n ---PLEASE CHECK AGAIN!---";
             break;
         }
     }
-
 };
-
-class Transaction
-{
-    private:
-        double deposit;
-        double withdraw;
-        double balance;
-        int option2;
-    public:
-        void transact()
-        {
-            switch (option2)
-            {
-            case 1:
-                cout << "Enter the amount of deposit: " << deposit << endl;
-                cin >> deposit;
-                cout << "You have successfully deposited "<< deposit << "!\n";
-                balance = balance + deposit;
-                break;
-            case 2:
-                cout << "Enter the amount of withdraw: "<< withdraw << endl;
-                cin >> withdraw;
-                cout << "You have successfully withdrawn "<< withdraw << "!\n";
-                balance = balance - withdraw;
-                break;
-            case 3:
-                cout << "Your remaining balance is: " << balance << endl;
-                break;
-            default:
-                cout << "INVALID INPUT! \n PLEASE CHECK AGAIN!"
-            }
-        }
-};
-
-/*int main()
-{
-    cout << "Hi! Welcome to ATM Machine!\n\n";
-    cout << "Please select an option from the menu below: \n";
-    Menu obj;
-    obj.selectMenu();
-    return 0;
-}*/
-
 
 int main()
 {
@@ -101,36 +178,5 @@ int main()
     Menu obj;
     obj.selectMenu();
 
-    Transaction txn;
-    txn.transact();
-
-  /*  ifstream accInFile;
-    ofstream accOutFile;
-
-    accInFile.open("account_1.txt");
-
-    char input;
-    if (!accInFile.fail())
-        {
-        cout << " overwrite with the new data (y or n): ";
-        cin >> input;
-        if (tolower(input) == 'n')
-            {
-            cout << "The existing file will not be overwritten." << endl;
-            exit(1);
-            }
-        }
-        accOutFile.open("account_1.txt");
-
-
-    accOutFile.open("account_1.txt");
-    if (accInFile.fail())
-            {
-            cout << "\nThe file was not successfully opened"
-            << endl;
-            exit(1);
-            }
-            cout << "The file has been successfully opened for output."
-            << endl;*/
-return 0;
+    return 0;
 }
