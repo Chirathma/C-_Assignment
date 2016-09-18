3e740f56ef003595102917d258175094a7565ac8
Devni-C <devniperera14@gmail.com>
2016-09-18 20:28:59 +0530

    Correct code :|

--- main.cpp
+++ main.cpp
@@ -1,16 +1,156 @@
-// This program demonstrates the declaration of an fstream
-// object and the opening of a file.
 #include <iostream>
-#include <fstream>
+#include <string>
 using namespace std;
 
-int main(void)
+
+class Menu
 {
-	fstream dataFile; // Declare file stream object
-	char fileName[81];
-	cout << "Enter the name of a file you wish to open\n";
-	cout << "or create: ";
-	cin.getline(fileName, 81);
-	dataFile.open(fileName, ios::out);
-	cout << "The file " << fileName << " was opened.\n";
+private:
+    string userID; //User ID: It can be contained string characters
+    int pwd; //Password: It must contain only integer numbers
+    int option1;
+
+    void login()
+    {
+        cout << "---Login---\n\n";
+        cout << "Enter User ID: ";
+        cin >> userID;
+        cout << "\nEnter Password: ";
+        cin >> pwd;
+    }
+    void createAcc()
+        {
+            cout << "---Create New Account---\n\n";
+            cout << "Enter User ID: ";
+            cin >> userID;
+            cout << "\nEnter the password: ";
+            cin >> pwd;
+            cout << "\n Re-enter the password: ";
+            cin >> pwd;
+        }
+
+    void quitAcc()
+    {
+        cout << "~~Thank You! Come Again!~~\n\n";
+    }
+
+public:
+    int option;
+    string acc_no;
+
+    void select()
+    {
+        cout << "1 - Login \n2 - Create New Account \n3 - Quit\n";
+        cin >> option;
+        switch (option)
+        {
+        case 1:
+            login();
+            break;
+
+        case 2:
+            createAcc();
+            break;
+
+        case 3:
+            quitAcc();
+            break;
+
+        default:
+            cout << "--INVALID INPUT!-- \n ---PLEASE CHECK AGAIN!---";
+            break;
+        }
+    }
+
+};
+
+
+//class Account : public Menu
+//class sav_acc
+class Account
+{
+public:
+    int option;
+    float deposit;
+    float withdraw;
+    float balance=10000;
+
+
+    /*void accDetails()
+    {
+        //string acc_no;
+        cout<< "\nEnter your account number: ";
+        cin>> acc_no;
+    }*/
+
+    bool anyTxn()
+    {
+        char response;
+        cout<< "\nAny Transaction? Y / N\n";
+        cin>> response;
+        if(response=='Y'|response=='y')
+        {
+            return true;
+        }
+        else
+        {
+            cout<< "\n\"--THANK YOU, COME AGAIN!-- \"\n";
+            return false;
+        }
+    }
+
+    void display()
+    {
+        Menu menu;
+        cout<< "\nEnter your requirement: "
+            << "\n1 - Balance \n2 - Deposit \n3 - Withdraw \n4 - Main Menu \n\n";
+        cin>> option;
+        switch(option)
+        {
+            case 1:
+                cout<< "\nYour balance is  "<<balance<< endl;
+                break;
+
+            case 2:
+                cout<< "\nEnter the amount of deposit: ";
+                cin>> deposit;
+                balance += deposit;
+                break;
+
+            case 3:
+                cout<< "\nEnter the amount of withdraw: ";
+                cin>> withdraw;
+                balance -= withdraw;
+                break;
+            case 4:
+                menu.select();
+
+            default:
+                cout<< "\n--INVALID INPUT !!!--\n";
+        }
+
+            if (anyTxn())
+            {
+                display();
+            }
+            else
+                cout<< "\n\"--THANK YOU, COME AGAIN!-- \"\n";
+
+    }
+    };
+
+
+
+int main()
+{
+    cout << "Hi! Welcome to ATM Machine!\n\n";
+    cout << "Please select an option from the menu below: \n";
+
+    Account accHolder;
+    Menu menu;
+    menu.select();
+    //accHolder.accDetails();
+    accHolder.display();
+    return 0;
+
 }
