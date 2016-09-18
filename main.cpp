2e4ab1616e914408ef158e7e60eafdb21bd99202
Devni-C <devniperera14@gmail.com>
2016-09-18 20:17:59 +0530

    new ATM code

--- main.cpp
+++ main.cpp
@@ -1,154 +1,16 @@
-#include<iostream>
-#include<fstream>
-#include<string.h>
-#include <cstdlib> // used to exit
+// This program demonstrates the declaration of an fstream
+// object and the opening of a file.
+#include <iostream>
+#include <fstream>
 using namespace std;
 
-//account details and
-class Account
+int main(void)
 {
-private:
-    string userId;
-    double balance;
-public:
-    Account(string id, double balanceValue)
-    {
-        userId = id;
-        balance = balanceValue;
-    }
-    double getBalance ()
-    {
-        return balance;
-    }
-    void deposit(double amount)
-    {
-        balance += amount;
-    }
-
-    bool withdraw(double amount)
-    {
-        if(amount < balance)
-        {
-            balance -= amount;
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-};
-
-//selecting main menu
-class Menu
-{
-private:
-    string userID; //User ID: It can be contained string characters
-    int pwd; //Password: It must contain only integer numbers
-    int option, option1; //used as the switch-case operator
-    double withdrawValue;
-    double depositValue;
-    double balance;
-
-    Account login()
-    {
-        cout << "---Login---\n\n";
-        cout << "Enter ID: ";
-        cin >> userID;
-        cout << "\nEnter Password: ";
-        cin >> pwd;
-
-        //Read the file named userID
-        //Match password
-        //If password is correct, create an Account object
-        double balance = 1500;
-        Account account(userID, balance);
-        return account;
-    }
-
-    void createAcc()
-    {
-        cout << "---Create New Account---\n\n";
-        cout << "Enter User ID: ";
-        cin >> userID;
-        cout << "\nEnter the password: ";
-        cin >> pwd;
-        cout << "\n Re-enter the password: ";
-        cin >> pwd;
-    }
-
-    void quitAcc()
-    {
-        cout << "~~Thank You! Come Again!~~\n\n";
-    }
-
-public:
-    void selectMenu()
-    {
-        cout << "1 - Login \n2 - Create New Account \n3 - Quit\n";
-        cin >> option1;
-        int option;
-        switch (option1)
-        {
-        case 1:
-        {
-            Account account= login();
-            cout<< "\nEnter your requirement: "
-                << "\nBalance-'1'\t Deposit-'2'\t Withdraw-'3'\n";
-            cin>> option;
-            switch(option)
-            {
-            case 1:
-            {
-                balance = account.getBalance();
-                cout<< "\nYour balance is  "<<balance<< endl;
-            }
-            break;
-
-            case 2:
-                cout<< "\nEnter the amount of deposit: ";
-                cin>> depositValue;
-                account.deposit(depositValue);
-                cout << "\nYour balance is  "<<account.getBalance()<< endl;
-                break;
-
-            case 3:
-                cout<< "\nEnter the amount of withdraw: ";
-                cin>> withdrawValue;
-                account.withdraw(withdrawValue);
-                cout << "\nYour balance is  "<<account.getBalance()<< endl;
-                break;
-
-            default:
-                cout<< "\n--INVALID INPUT !!!--\n";
-            }
-        }
-        selectMenu();
-        break;
-
-        case 2:
-            createAcc();
-            selectMenu();
-            break;
-
-        case 3:
-            quitAcc();
-            break;
-
-        default:
-            cout << "--INVALID INPUT!-- \n ---PLEASE CHECK AGAIN!---";
-            selectMenu();
-            break;
-        }
-    }
-};
-
-int main()
-{
-    cout << "Hi! Welcome to ATM Machine!\n\n";
-    cout << "Please select an option from the menu below: \n";
-    Menu obj;
-    obj.selectMenu();
-
-    return 0;
+	fstream dataFile; // Declare file stream object
+	char fileName[81];
+	cout << "Enter the name of a file you wish to open\n";
+	cout << "or create: ";
+	cin.getline(fileName, 81);
+	dataFile.open(fileName, ios::out);
+	cout << "The file " << fileName << " was opened.\n";
 }
